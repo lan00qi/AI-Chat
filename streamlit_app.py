a88d1d47dea9a1182c903292d80fce53abad277d
lan00qi <lanqili9@gmail.com>
2025-05-23 00:29:34 +0000

    Completed project

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,19 +13,27 @@ if "chat_history" not in st.session_state:
     {"role": "system","content": system_prompt},
     
     ]
+
+for chat in st.session_state["chat_history"]:
+    if chat["role"] == "assistant":
+        message(chat["content"])
+    elif chat["role"] == "user":
+        message(chat["content"], is_user = True )
+    
+        
+
 with st.form("chat"):
     user_messages = st.text_input("Message")
     submitted = st.form_submit_button("send")
     if submitted and user_messages:
         st.session_state["chat_history"].append({"role":"user","content": user_messages})
-        reponse = client.chat.completions.create(
+        response = client.chat.completions.create(
             model = "gpt-3.5-turbo-0125",
             messages = st.session_state["chat_history"]
+        
             
         )
-
-st.write(st.session_state["chat_history"])
-        
+        st.session_state["chat_history"].append({"role":"assistant","content": response.choices[0].message.content})
 
 
 
